4a0d91a6fbe5dbcb7dd571fb572f0791d8cbd4da
luiz-gabriel <luizgabriel515@gmail.com>
2020-11-26 17:16:25 -0300

    Aula sobre variaveis mutaveis e imutaveis

---
+++ variaveis-mutaveis-imutaveis.kt
@@ -0,0 +1,27 @@
+fun main()
+{
+   /** Variaveis mutaveis são variaveis que uma vez inicializada
+   * pode ser alterada. Ex:
+   * var nome: String = "Luiz"; 
+   * nome = "Gabriel";
+   *
+   * Variaveis imutaveis não podem receber um valor, uma vez 
+   * que já recebeu.
+   
+   * Ex:
+   * val idade: Int = 11;
+   * idade = 12;
+   *
+   * Esse exemplo gera o seguinte erro:
+   * error: val cannot be reassigned
+   * idade = 12;
+   * ^
+   *
+   * Forma diferente de declara um val, definindo o tipo da variavel. 
+   * Pois assim o kotlin sabe qual o tamanho dá variavel.
+   * val x: Int
+   * x = 1;
+   */
+
+   
+}
