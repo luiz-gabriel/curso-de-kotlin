cb0e4affa453a73742d3ba54d2d7f4d62794e1d5
luiz-gabriel <luizgabriel515@gmail.com>
2020-11-26 17:09:16 -0300

    Aula sobre tipos de dados e seus tamanhos.

---
+++ tipos-de-dados.kt
@@ -0,0 +1,29 @@
+/*
+   Tipos de dados:
+
+   Type Bit
+   Double 64
+   Float 32
+   Long 64
+   Int 32
+   Short 16
+   Boolean ?
+   String ?
+   Char ?
+
+   Unidades de medidas
+   1 byte = 0 bits
+   1 kilobyte (kb) = 1024 bytes
+   1 megabyte (Mb) = 1024 kilobytes
+   1 gigabyte (Gb) = 1024 megabytes
+*/
+
+fun main() 
+{
+   println("Double: Max: " + Double.MAX_VALUE + " Min:" + Double.MIN_VALUE);
+   println("Float: Max: " + Float.MAX_VALUE + " Min: " + Float.MIN_VALUE);
+   println("Long: Max: " + Long.MAX_VALUE + " Min: " + Long.MIN_VALUE);
+   println("Int: Max: " + Int.MAX_VALUE + " Min: " + Int.MIN_VALUE);
+   println("Short: Max: " + Short.MAX_VALUE + " Min: " + Short.MIN_VALUE);
+   println("Byte: Max: " + Byte.MAX_VALUE + " Min: " + Byte.MIN_VALUE);
+}
