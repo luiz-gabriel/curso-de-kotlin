e320a0a49a2c671a617e2b659c333d3b01c1f2e6
luiz-gabriel <luizgabriel515@gmail.com>
2020-11-28 13:06:16 -0300

    Formataçao de strings

---
+++ formatacao-de-strings.kt
@@ -0,0 +1,14 @@
+fun main(){
+   val frase = "é uma linguagem";
+   val carac = "show";
+   
+   println("Kotlin ${frase.length} $carac");
+   
+   val str = """
+   fsadfsadf
+   dsfasdf
+   asdfas
+   """.trimMargin()
+
+   println(str.length);
+}
