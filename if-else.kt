f96465a5a9e00eb5020f4bd139c2d904037bf1ae
luiz-gabriel <luizgabriel515@gmail.com>
2020-12-02 11:11:43 -0300

    Iniciando em controle de fluxo

---
+++ if-else.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+   
+   println(idade(18))
+}
+
+fun idade(idade:Int) : Boolean
+{
+   return idade >= 18
+  
+   // if(idade >= 18)
+   // {
+   //    println("Você é maior de idade")
+   // }else{
+   //    println("Você não é maior de idade")
+   // }
+}
